8112f15868b2f0386ada20847e294208071a9723
RaphaelLu <ronilu66@hotmail.com>
2016-08-01 23:40:29 +0900

    base of django

---
+++ img_site/img_site/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from . import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'img_site.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index')
+ 	# url(r'^img_site/', include('img_site.urls'))
+       
+]
